52a40fd4839e76b9a23cadf82b6b211e1bdcdb1a
Md___Emon <ishtiaqueferdous@nextgenitltd.com>
2023-08-05 11:30:31 +0600

    string methods

---
+++ stringMethods.js
@@ -0,0 +1,17 @@
+let myName = "Md Ishtiaque Ferdous Emon"
+
+/**
+ * string length
+ */
+console.log(myName.length);
+
+/**
+ * uppercase & lowercase
+ */
+console.log(myName.toUpperCase(), myName.toLowerCase())
+
+/**
+ * string concate
+ */
+
+console.log(myName + " is a laravel developer")
