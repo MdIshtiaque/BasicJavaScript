a293db5b13405034e1ce04fd2f68f043de848c19
Md___Emon <ishtiaqueferdous@nextgenitltd.com>
2023-08-05 11:13:00 +0600

    math object, type Conversion

---
+++ mathObjectAndNumber.js
@@ -0,0 +1,18 @@
+let x = 4.8732;
+
+console.log(Math.round(x), Math.ceil(x), Math.floor(x), Math.trunc(x));
+
+/**
+ * positive or negative
+ */
+console.log(Math.sign(x));
+
+/***
+ * Random Default 0 - 1
+ */
+
+console.log(Math.random());
+
+
+console.log(Math.trunc(Math.random()*10000));  // OTP generation
+
---
+++ typeConversion.js
@@ -0,0 +1,3 @@
+
+let pi = "3.1416";
+console.log(typeof(pi), typeof(Number(pi)), Number(""), Number("E"), String(510+10), String(510+"10"));
