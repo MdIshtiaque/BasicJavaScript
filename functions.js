49b88bd475b1582b7c64a2a49438dfaa7b700cf7
Md___Emon <ishtiaqueferdous@nextgenitltd.com>
2023-08-06 15:43:36 +0600

    Functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+/**
+ * Function
+ */
+
+function doublefunction(num){
+    return num * 2;
+}
+
+console.log(doublefunction(10));
+
+
+/***
+ *
+ * ES6 functions & arrow functions
+ */
+
+let double = (num) => {
+  return num * 2;
+}
+
+console.log(double(50));
